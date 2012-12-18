d9b66f78b39ec0b5ecca6d5adc0e927ffbb8d6f1
kadirpekel <kadirpekel@gmail.com>
2012-12-18 15:11:02 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='komandr',
     py_modules=['komandr'],
-    version='0.0.1',
+    version='0.0.4',
     description='smart command-line interface',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
