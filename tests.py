2fa35e35b2dfa69444be763e3d6539e8c70e621d
Dalton Barreto <daltonmatos@gmail.com>
2013-03-02 23:40:47 -0300

    Add arg_list to the __call__ method of prog class.
    This way we can choose which arguments komandr will parse, not always
    we want to use sys.argv[1:].

--- tests.py
+++ tests.py
@@ -12,6 +12,14 @@ class TestKomandr(unittest.TestCase):
         self.assertEqual(('1', '2'),
                          komandr.execute(['foo', '1', '--baz', '2']))
 
+    def testCustomArgsTobeParsed(self):
+        _newParser = komandr.prog()
+        def foo(bar, baz=None):
+            return bar, baz
+        _newParser.command(foo)
+        self.assertEqual(('1', '2'),
+                         _newParser(['foo', '1', '--baz', '2']))
+
 
 if __name__ == '__main__':
     unittest.main()
